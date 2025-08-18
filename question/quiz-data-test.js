afadaddfa73f44c24d3c59a20d00e769f246f029
kokzilla <chartwut@gmail.com>
2025-08-18 07:44:46 +0700

    Create quiz-data-test.js

---
+++ question/quiz-data-test.js
@@ -0,0 +1,24 @@
+const quizData = [
+    { // -- 1
+        questionFormat: 1,
+        question: "สิ่งมีชีวิตชนิดใดสังเคราะห์แสงได้",
+        answerOptions: [
+            { text: "เฟิร์น", isCorrect: true },
+            { text: "เห็ดนางฟ้า", isCorrect: false },
+            { text: "ยุงลาย", isCorrect: false },
+            { text: "เชื้อไวรัสโคโรน่า", isCorrect: false }
+        ]
+    },
+    {
+        questionFormat: 1,
+        question: "45 นาที เท่ากับกี่ชั่วโมง",
+        answerOptions: [
+            { text: "0.25 ชั่วโมง", isCorrect: false },
+            { text: "0.5 ชั่วโมง", isCorrect: false },
+            { text: "0.75 ชั่วโมง", isCorrect: true },
+            { text: "1 ชั่วโมง", isCorrect: false }
+        ]
+    }    
+];
+
+export default quizData;
